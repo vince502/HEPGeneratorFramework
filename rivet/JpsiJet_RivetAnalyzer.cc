fa7e6ff1be63f05733ea840aeba92ab22a656d2a
soohwan <soohwan@adminui-MacBookPro.local>
2025-12-26 18:07:55 +0900

    Fix Rivet 4 compilation errors and FastJet enum compatibility in analysis plugin

--- rivet/JpsiJet_RivetAnalyzer.cc
+++ rivet/JpsiJet_RivetAnalyzer.cc
@@ -1,6 +1,6 @@
 // -*- C++ -*-
 #include "Rivet/Analysis.hh"
-//#include "Rivet/Projections/DressedLeptons.hh"
+// #include "Rivet/Projections/DressedLeptons.hh"
 #include "Rivet/Projections/FastJets.hh"
 #include "Rivet/Projections/FinalState.hh"
 #include "Rivet/Projections/MissingMomentum.hh"
@@ -25,26 +25,24 @@ namespace Rivet {
 /// @brief Add a short analysis description here
 class JpsiJet_RivetAnalyzer : public Analysis {
 public:
-  /// Constructor
-  DEFAULT_RIVET_ANALYSIS_CTOR(JpsiJet_RivetAnalyzer);
+  // Constructor
+  RIVET_DEFAULT_ANALYSIS_CTOR(JpsiJet_RivetAnalyzer);
 
   void init() {
 
     // Projections
     FinalState fs(Cuts::abseta < 5.0);
-    //declare(UnstableParticles(), "UFS");
-    //      FastJets jet4(fs, FastJets::ANTIKT, 0.4); // R = 0.4 jets
-    FastJets jet4(fs, FastJets::ANTIKT, 0.4, JetAlg::Muons::NONE, JetAlg::Invisibles::NONE); // R = 0.8 jets
+    FastJets jet4(fs, JetAlg::ANTIKT, 0.4);
     declare(jet4, "jet4");
 
     // Particles for the jets
     VetoedFinalState jet_input(fs);
-    const UnstableParticles jpsi_fs =
-        UnstableParticles(Cuts::abspid == PID::JPSI && Cuts::abseta < 2.4 &&
-    //FinalState jpsi_fs =
-    //    FinalState(Cuts::abspid == PID::JPSI && Cuts::abseta < 2.4 &&
-                   Cuts::pT > 6.5 * GeV &&
-                   Cuts::pT < 30 * GeV); // Promptfinalstate for jpsi
+    const UnstableParticles jpsi_fs = UnstableParticles(
+        Cuts::abspid == PID::JPSI && Cuts::abseta < 2.4 &&
+        // FinalState jpsi_fs =
+        //     FinalState(Cuts::abspid == PID::JPSI && Cuts::abseta < 2.4 &&
+        Cuts::pT > 6.5 * GeV &&
+        Cuts::pT < 30 * GeV); // Promptfinalstate for jpsi
     declare(jpsi_fs, "Jpsi");
     jet_input.vetoNeutrinos(); // veto neutrinos
     declare(jet_input, "JET_INPUT");
@@ -53,11 +51,17 @@ public:
     // instance
     book(_njets, "_njets");
 
-    //book(_hZ, "zJpsi", {0.16, 0.22, 0.376, 0.532,  0.688, 0.844, 1.0});
+    // book(_hZ, "zJpsi", {0.16, 0.22, 0.376, 0.532,  0.688, 0.844, 1.0});
     book(_hnJpsi, "nJpsi", {0, 1, 2, 3, 4});
-    book(_hZ, "zJpsi", {0.16, 0.22, 0.298, 0.376, 0.454, 0.532, 0.610,  0.688, 0.766, 0.844, 0.922, 1.0});
-    book(_hZqqcc, "zJpsiFromqqbar", {0.16, 0.22, 0.298, 0.376, 0.454, 0.532, 0.610,  0.688, 0.766, 0.844, 0.922, 1.0});
-    book(_hZgcc, "zJpsiFromgtocc", {0.16, 0.22, 0.298, 0.376, 0.454, 0.532, 0.610,  0.688, 0.766, 0.844, 0.922, 1.0});
+    book(_hZ, "zJpsi",
+         {0.16, 0.22, 0.298, 0.376, 0.454, 0.532, 0.610, 0.688, 0.766, 0.844,
+          0.922, 1.0});
+    book(_hZqqcc, "zJpsiFromqqbar",
+         {0.16, 0.22, 0.298, 0.376, 0.454, 0.532, 0.610, 0.688, 0.766, 0.844,
+          0.922, 1.0});
+    book(_hZgcc, "zJpsiFromgtocc",
+         {0.16, 0.22, 0.298, 0.376, 0.454, 0.532, 0.610, 0.688, 0.766, 0.844,
+          0.922, 1.0});
 
     book(_hJpsipT, "JpsipT", {5, 6, 8, 10, 15, 20, 25, 30});
     book(_hJetpT, "JetpT", {5, 10, 15, 20, 30, 40, 50, 70, 100});
@@ -65,70 +69,83 @@ public:
 
   void analyze(const Event &event) {
 #ifdef JpsiDEBUG
-    printf("Start event\n" );
+    printf("Start event\n");
 #endif
-    //const UnstableParticles& unstable = apply<UnstableParticles>(event, "UFS");
+    // const UnstableParticles& unstable = apply<UnstableParticles>(event,
+    // "UFS");
 
     const VetoedFinalState &fs = apply<VetoedFinalState>(event, "JET_INPUT");
     const Particles &fsParticles = fs.particles();
-//    const Particles &jpsi_particles =
-//        apply<FinalState>(event, "Jpsi").particles();
+    //    const Particles &jpsi_particles =
+    //        apply<FinalState>(event, "Jpsi").particles();
 
-    const Particles& jpsi_particles = apply<UnstableParticles>(event, "Jpsi").particles();
-    if (jpsi_particles.size() == 0) vetoEvent;
+    const Particles &jpsi_particles =
+        apply<UnstableParticles>(event, "Jpsi").particles();
+    if (jpsi_particles.size() == 0)
+      vetoEvent;
     bool isNonPrompt = jpsi_particles[0].fromBottom();
-    if( isNonPrompt ) vetoEvent;
+    if (isNonPrompt)
+      vetoEvent;
 
-//    Jets jets = apply<FastJets>(event, "jet4").jetsByPt(Cuts::pT > 30 * GeV).jetsByPt(Cuts::pT < 40 * GeV);
+    //    Jets jets = apply<FastJets>(event, "jet4").jetsByPt(Cuts::pT > 30 *
+    //    GeV).jetsByPt(Cuts::pT < 40 * GeV);
 
 #ifdef JpsiDEBUG
-    printf("Pass Onia Veto!!\n" );
+    printf("Pass Onia Veto!!\n");
 #endif
     _hnJpsi->fill(std::min(jpsi_particles.size(), (long unsigned int)5));
     double pTj = jpsi_particles[0].pt();
     _hJpsipT->fill(pTj);
 
     vector<PseudoJet> toCheck;
-    for (const Particle& daughter : jpsi_particles) {
-       for(const Particle& desc : daughter.children()){
-         toCheck.push_back(desc.pseudojet());
- //        printf("Dau : %d %f %f\n",desc.pid(), desc.eta(), desc.phi());
-       }
+    for (const Particle &daughter : jpsi_particles) {
+      for (const Particle &desc : daughter.children()) {
+        toCheck.push_back(desc.pseudojet());
+        //        printf("Dau : %d %f %f\n",desc.pid(), desc.eta(), desc.phi());
+      }
     }
 
     vector<PseudoJet> particles;
     particles.reserve(fsParticles.size());
     for (uint iFS = 0; iFS < fsParticles.size(); iFS++) {
       PseudoJet p = fsParticles[iFS].pseudojet();
-//      if(fsParticles[iFS].abspid() == 13 )printf("pseudo jet : %d, %f %f\n",fsParticles[iFS].pid(), fsParticles[iFS].eta(), fsParticles[iFS].phi() );
-      for( auto dau : toCheck){
-        if(fabs((fsParticles[iFS].eta() - dau.eta()) <1e-4) && fabs(fsParticles[iFS].phi() - dau.phi()) < 1e-4) p.set_user_index(1313); // FastJet index used to keep track of J/psi
-//       if(fsParticles[iFS].abspid() == 13 ) printf("Check : %d\n", p.user_index());
+      //      if(fsParticles[iFS].abspid() == 13 )printf("pseudo jet : %d, %f
+      //      %f\n",fsParticles[iFS].pid(), fsParticles[iFS].eta(),
+      //      fsParticles[iFS].phi() );
+      for (auto dau : toCheck) {
+        if (fabs((fsParticles[iFS].eta() - dau.eta()) < 1e-4) &&
+            fabs(fsParticles[iFS].phi() - dau.phi()) < 1e-4)
+          p.set_user_index(1313); // FastJet index used to keep track of J/psi
+        //       if(fsParticles[iFS].abspid() == 13 ) printf("Check : %d\n",
+        //       p.user_index());
       }
       particles.push_back(p);
     }
 
-    JetDefinition jetDefAKT_Sig(fastjet::antikt_algorithm, 0.4); // jet distance parameter
+    JetDefinition jetDefAKT_Sig(fastjet::antikt_algorithm,
+                                0.4); // jet distance parameter
     ClusterSequence antikTjets(particles, jetDefAKT_Sig); // cluster anti-kT
-    vector<PseudoJet> jets = fastjet::sorted_by_pt(antikTjets.inclusive_jets(30)); // Jet projection : we only keep jets with pT > 30 Gev
+    vector<PseudoJet> jets = fastjet::sorted_by_pt(antikTjets.inclusive_jets(
+        30)); // Jet projection : we only keep jets with pT > 30 Gev
 
-  //  printf("Pseudo jet size : %d, Clustered jet size : %d\n", (int) particles.size(), (int) jets.size());
+    //  printf("Pseudo jet size : %d, Clustered jet size : %d\n", (int)
+    //  particles.size(), (int) jets.size());
 
     if (jets.size() < 1)
       vetoEvent; // at least 1 jet in the event
-   _hJetpT->fill(jets[0].pt());
-    printf("Pass Jet Veto!!\n" );
+    _hJetpT->fill(jets[0].pt());
+    printf("Pass Jet Veto!!\n");
     if (jpsi_particles.size() != 1)
       vetoEvent; // 1 Jpsi in the final state (do we allow the finalstate to
                  // have more than 1 Jpsi ? Maybe it will be interesting to
                  // modify the routine so that you can treat the case of
                  // multiple Jpsi productions)
 #ifdef JpsiDEBUG
-    printf("Pass Unique Onia Veto!!\n" );
+    printf("Pass Unique Onia Veto!!\n");
 #endif
     int nJets = 0;
     const double maxDeltaRsquare = 0.4; // Define the maximum value of deltaR
-                                         // for the Jpsi to be in the Jet
+                                        // for the Jpsi to be in the Jet
     for (size_t i = 0; i < jets.size(); ++i) // loop over all jets
     {
       // Check if the Jpsi is in the jet (should be changed in case we allow
@@ -139,37 +156,38 @@ public:
       double deltaRsquare = deltaPhi + deltaEta;
       std::vector<PseudoJet> constituents = jets[i].constituents();
       int hasJpsi = 0;
-      for (const auto& constituent : constituents) {
-        if(constituent.user_index() == 1313)hasJpsi += 1;
+      for (const auto &constituent : constituents) {
+        if (constituent.user_index() == 1313)
+          hasJpsi += 1;
       }
 #ifdef JpsiDEBUG
-      printf("Matching muon size %d\n", hasJpsi );
+      printf("Matching muon size %d\n", hasJpsi);
 #endif
-      if(hasJpsi<2)
+      if (hasJpsi < 2)
         continue;
 #ifdef JpsiDEBUG
-      printf("Pass Jet has onia!!\n" );
+      printf("Pass Jet has onia!!\n");
 #endif
       if (deltaRsquare > maxDeltaRsquare)
         continue;
 #ifdef JpsiDEBUG
-      printf("Pass Jet dR!!\n" );
+      printf("Pass Jet dR!!\n");
 #endif
       if (fabs(jets[i].pt()) > 40)
         continue;
 #ifdef JpsiDEBUG
-      printf("Pass Jet pt 30-40!!\n" );
+      printf("Pass Jet pt 30-40!!\n");
 #endif
       if (fabs(jets[i].pseudorapidity()) > 2.)
         continue; // jet acceptance cut (in the CMS article, pseudo-rapidity
                   // should be smaller than 2)
 #ifdef JpsiDEBUG
-      printf("Pass Jet Match!!\n" );
+      printf("Pass Jet Match!!\n");
 #endif
       if (nJets > 0)
         continue;
 #ifdef JpsiDEBUG
-      printf("Only first jet!!\n" );
+      printf("Only first jet!!\n");
 #endif
 
       nJets++; // count jets : the previous condition ensures that there is only
@@ -177,9 +195,11 @@ public:
       double z = jpsi_particles[0].pt() / jets[i].pt();
       printf("z value: %f\n", z);
       assert(z > 0);
-      if(isFromGtoCC(jpsi_particles[0])) _hZgcc->fill(z);
-      else _hZqqcc->fill(z);
-      
+      if (isFromGtoCC(jpsi_particles[0]))
+        _hZgcc->fill(z);
+      else
+        _hZqqcc->fill(z);
+
       _hZ->fill(z);
 
       _njets->fill(nJets);
@@ -195,39 +215,39 @@ private:
   Histo1DPtr _hJpsipT;
   Histo1DPtr _hJetpT;
   CounterPtr _njets;
-bool isFromGtoCC(const Particle& p) const {
+  bool isFromGtoCC(const Particle &p) const {
     // Start with the current particle
     std::vector<Particle> ancestors = {p};
 
     // Traverse the ancestry
     while (!ancestors.empty()) {
-        const Particle current = ancestors.back();
-//    	printf("Starting with PID : %d", current.pid());
-        ancestors.pop_back();
+      const Particle current = ancestors.back();
+      //    	printf("Starting with PID : %d", current.pid());
+      ancestors.pop_back();
 
-        // Check if the current particle is a gluon
-        if (current.pid() == 21) {  // PDG ID 21 = gluon
-            int cCount = 0;
-            for (const Particle& child : current.children()) {
-//    		printf(" : child PID : %d\n", child.pid());
-                if (std::abs(child.pid()) == 4) { // PDG ID 4 = charm quark
-                    ++cCount;
-                }
-            }
-            if (cCount >= 2) {
-                return true;
-            }
+      // Check if the current particle is a gluon
+      if (current.pid() == 21) { // PDG ID 21 = gluon
+        int cCount = 0;
+        for (const Particle &child : current.children()) {
+          //    		printf(" : child PID : %d\n", child.pid());
+          if (std::abs(child.pid()) == 4) { // PDG ID 4 = charm quark
+            ++cCount;
+          }
         }
+        if (cCount >= 2) {
+          return true;
+        }
+      }
 
-        // Add the parents of the current particle to the list for further checking
-        for (const Particle& parent : current.parents()) {
-            ancestors.push_back(parent);
-        }
+      // Add the parents of the current particle to the list for further
+      // checking
+      for (const Particle &parent : current.parents()) {
+        ancestors.push_back(parent);
+      }
     }
     return false;
-}
+  }
 };
 
-DECLARE_RIVET_PLUGIN(JpsiJet_RivetAnalyzer);
-
+RIVET_DECLARE_PLUGIN(JpsiJet_RivetAnalyzer);
 } // namespace Rivet
